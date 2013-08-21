3b2baa5021eb2c73d3c7d362e10fb2a47372223b
wagnert <t.wagner@techdivision.com>
2013-08-21 17:29:35 +0200

    Switch to annotated beans instead of interfaces

--- src/META-INF/classes/TechDivision/Example/Services/UserProcessor.php
+++ src/META-INF/classes/TechDivision/Example/Services/UserProcessor.php
@@ -28,6 +28,8 @@ use Doctrine\ORM\Tools\SchemaValidator;
  * @license    	http://opensource.org/licenses/osl-3.0.php
  *              Open Software License (OSL 3.0)
  * @author      Tim Wagner <tw@techdivision.com>
+ * 
+ * @Stateless
  */
 class UserProcessor implements Stateless {
 
