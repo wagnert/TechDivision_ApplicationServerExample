68fb79d0014a75435fe6b4f21bcc7c4daaa7baf4
wagnert <wagner_tim78@hotmail.com>
2014-10-30 09:30:42 +0100

    Add HelloWorldServlet + specific JMeter testplans

---
+++ src/WEB-INF/classes/TechDivision/Example/Servlets/HelloWorldServlet.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * TechDivision\Example\Servlets\HelloWorldServlet
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * PHP version 5
+ *
+ * @category   Appserver
+ * @package    TechDivision_ApplicationServerExample
+ * @subpackage Handlers
+ * @author     Tim Wagner <tw@techdivision.com>
+ * @copyright  2014 TechDivision GmbH <info@techdivision.com>
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link       http://www.appserver.io
+ */
+
+namespace TechDivision\Example\Servlets;
+
+use TechDivision\Servlet\Http\HttpServlet;
+use TechDivision\Servlet\Http\HttpServletRequest;
+use TechDivision\Servlet\Http\HttpServletResponse;
+
+/**
+ * Demo servlet handling GET requests.
+ *
+ * @category   Appserver
+ * @package    TechDivision_ApplicationServerExample
+ * @subpackage Handlers
+ * @author     Tim Wagner <tw@techdivision.com>
+ * @copyright  2014 TechDivision GmbH <info@techdivision.com>
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link       http://www.appserver.io
+ */
+class HelloWorldServlet extends HttpServlet
+{
+
+    /**
+     * Handles a HTTP GET request.
+     *
+     * @param \TechDivision\Servlet\Http\HttpServletRequest  $servletRequest  The request instance
+     * @param \TechDivision\Servlet\Http\HttpServletResponse $servletResponse The response instance
+     *
+     * @return void
+     * @throws \TechDivision\Servlet\ServletException Is thrown if the request method is not implemented
+     * @see \TechDivision\Servlet\Http\HttpServlet::doGet()
+     */
+    public function doGet(HttpServletRequest $servletRequest, HttpServletResponse $servletResponse)
+    {
+        $servletResponse->appendBodyStream('Hello World!');
+    }
+}
